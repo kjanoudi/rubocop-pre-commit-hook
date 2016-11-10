056a8b62c8b54c243b8aae5347b4d5eb05415fae
Kareem Janoudi <kjanoudi@gmail.com>
2016-11-10 17:00:35 -0500

    test

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import setup
 
 setup(
     name='pre_commit_hooks',
-    description='rubocop-git pre commit hook.',
-    url='https://github.com/kjanoudi/rubocop-pre-commit-hook',
+    description='rubocop pre commit hook.',
+    url='https://github.com/jordant/rubocop-pre-commit-hook',
     version='0.1',
 
-    author='Kareem Janoudi',
-    author_email='kjanoudi@gmail.com',
+    author='Jordan Tardif',
+    author_email='jordan.tardif@gmail.com',
 
     packages=find_packages('.', exclude=('tests*', 'testing*')),
 
     entry_points={
         'console_scripts': [
-            'check-rubocop-git = pre_commit_hooks.check-rubocop-git:check-rubocop-git',
+            'check-rubocop = pre_commit_hooks.check_rubocop:check_rubocop',
         ],
     },
 )
